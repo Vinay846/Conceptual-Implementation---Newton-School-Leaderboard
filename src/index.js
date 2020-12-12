a9477215cd512efb655e2dfba99358578c4964f4
Vinay846 <Vinayvin84629@gmail.com>
2020-12-12 23:08:43 +0530

    Answer

--- src/index.js
+++ src/index.js
@@ -11,7 +11,29 @@ const { data } = require('./data')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+
+const isNullOrDefault = val => val === null || val === undefined;
+
 // your code goes here
+app.get("/topRankings", (req, res)=>{
+    let offset = parseInt(req.query.offset);
+    let limit = parseInt(req.query.limit);
+    console.log(offset);
+    if( offset < 0 || limit < 0){
+        offset = 0;
+        limit = 20;
+    }if(isNaN(offset)){
+        offset = 0;
+    }if(isNaN(limit)){
+        limit = 20;
+    }
+    let toSend = [];
+    for(let i=offset+1; i<=limit+offset; i++){
+        toSend.push(data[i]);
+    }
+
+    res.send(toSend);
+})
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
